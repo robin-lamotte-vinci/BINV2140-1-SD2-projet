a9576922dace778f42850935f2a1f842a2a90774
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-03-14 12:28:02 +0100

    update: Artiste constructor and equals method

--- src/Artiste.java
+++ src/Artiste.java
@@ -1,17 +1,26 @@
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Artiste {
 
     int id;
     String nom;
-    Set<String> categories;
+    String categories;
 
-    public Artiste(int id, String nom, Set<String> categories){
-        categories = new HashSet<>();
+    public Artiste(int id, String nom, String categories){
         this.id = id;
         this.nom = nom;
         this.categories = categories;
+    }
+
+    public Artiste(String str){
+        // Séparer les sous-chaînes par des virgules
+        String[] array = str.split(",");
+
+        this.id = Integer.parseInt(array[0]);
+        this.nom = array[1];
+        this.categories = array[2];
     }
 
     public String getNom() {
@@ -22,7 +31,21 @@ public class Artiste {
         return id;
     }
 
-    public Set<String> getCategories() {
+    public String getCategories() {
         return categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Artiste artiste = (Artiste) o;
+        return id == artiste.id && Objects.equals(nom, artiste.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom);
     }
 }
