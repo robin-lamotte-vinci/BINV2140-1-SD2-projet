4063f9416ffe9c73a41e12f437ed20d9b45de04e
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-03-28 18:19:32 +0100

    commit

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -3,17 +3,20 @@ import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Dijkstra {
 
   private final Artiste source;
   private final Artiste destination;
 
-  private Map<Artiste, Set<Mention>> mentionsSortantes;
+  private final Map<Artiste, Set<Mention>> mentionsSortantes;
 
-  private Map<Artiste, Double> etiquettesProvisoires;
-  private Map<Artiste, Double> etiquettesDefinitives;
+  private final Map<Artiste, Double> etiquettesProvisoires;
+  private final Map<Artiste, Double> etiquettesDefinitives;
+  TreeSet<Etiquette> queue ;
 
   Map<Artiste, Artiste> chemins;
 
@@ -22,6 +25,17 @@ public class Dijkstra {
     this.source = source;
     this.destination = destination;
 
+    this.etiquettesDefinitives = new HashMap<>();
+    this.etiquettesProvisoires = new HashMap<>();
+
+    this.chemins = new HashMap<>();
+    //this.queue = new TreeSet<>(Comparator.comparingDouble(Etiquette::getPoids));
+
+    this.queue = new TreeSet<>(
+        Comparator.comparingDouble(
+            etiquette -> etiquette.getPoids() != null ? etiquette.getPoids() : Double.MAX_VALUE
+        )
+    );
 
     trouverCheminMaxMentions();
     afficherCheminMaxMentions();
@@ -33,19 +47,18 @@ public class Dijkstra {
   }
 
   private void trouverCheminMaxMentions(){
-    this.etiquettesDefinitives = new HashMap<>();
-    this.etiquettesProvisoires = new HashMap<>();
 
-    this.chemins = new HashMap<>();
 
     //this.cheminsProvisoires = new TreeMap<>((a1, a2) -> Double.compare(etiquettesProvisoires.getOrDefault(a1, Double.MAX_VALUE), etiquettesProvisoires.getOrDefault(a2, Double.MAX_VALUE)));
 
     etiquettesProvisoires.put(source, 0.0);
+    //queue.add(new Etiquette(source, 0.0));
 
     Artiste artisteCourant = source;
 
     while (!destination.equals(artisteCourant) && !etiquettesProvisoires.isEmpty()){
       // trouver l'artiste de poids min
+
       Artiste a = etiquettesProvisoires
           .keySet()
           .stream()
@@ -53,10 +66,13 @@ public class Dijkstra {
           .orElseThrow(() -> new IllegalStateException(
               "La collection ne devrait jamais être vide à ce stade"));
 
+
+      //Etiquette e = queue.pollFirst();
       artisteCourant = a;
 
       etiquettesDefinitives.put(artisteCourant, etiquettesProvisoires.get(artisteCourant));
       etiquettesProvisoires.remove(artisteCourant);
+      //queue.remove(new Etiquette(artisteCourant, null));
 
       if (destination.equals(artisteCourant)) break;
 
@@ -66,11 +82,16 @@ public class Dijkstra {
           continue;
         }
 
+        if (etiquettesDefinitives.get(artisteCourant) == null) {
+          System.out.println("OHO");
+        }
         double nouveauPoids = etiquettesDefinitives.get(artisteCourant) + poidsMention(mention);
         double ancienPoids = etiquettesProvisoires.getOrDefault(artisteVoisin, Double.MAX_VALUE);
         if (nouveauPoids < ancienPoids){
-          etiquettesProvisoires.remove(artisteVoisin); //probablement inutile
+          //etiquettesProvisoires.remove(artisteVoisin); //probablement inutile
+          //queue.remove(new Etiquette(artisteVoisin, null)); //probablement inutile
           etiquettesProvisoires.put(artisteVoisin, nouveauPoids);
+          //queue.add(new Etiquette(artisteVoisin, nouveauPoids));
           chemins.put(artisteVoisin, artisteCourant);
         }
       }
@@ -103,4 +124,42 @@ public class Dijkstra {
     }
 
   }
+
+
+  static class Etiquette {
+    private final Artiste artiste;
+    private Double poids;
+
+    public Etiquette(Artiste artiste, Double poids) {
+      this.artiste = artiste;
+      this.poids = poids;
+    }
+
+    public Artiste getArtiste() {
+      return artiste;
+    }
+
+    public Double getPoids() {
+      return poids;
+    }
+
+    public void setPoids(Double poids) {
+      this.poids = poids;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Etiquette etiquette = (Etiquette) o;
+      return Objects.equals(artiste, etiquette.artiste);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(artiste);
+    }
+  }
+
 }
